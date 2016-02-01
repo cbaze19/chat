c8e0d75f6d976c8f1630cd7a2dfaafff9d5389dd
Caleb Baze <cbaze19@gmail.com>
2016-02-01 15:32:58 -0600

    blah

--- index.js
+++ index.js
@@ -31,38 +31,47 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-var users = {};
+
+var users = [];
+var history = [];
+
 
 // Main connection handler
 io.on('connection', function(socket) {
 
 	var clientIP = socket.request.connection.remoteAddress;
-
 	var userAdded = false;
+	var currRoom = '';
 
-	socket.on('new user', function(uname) {
 
-		if (!userAdded) {
+	socket.on('new user', function(name) {
+		socket.name = name;
 
-			socket.username = uname;
-
-			users[uname] = socket.username;
-
-			userAdded = true;
-			console.log(socket.username + ' has connected from ' + clientIP);
-			io.emit('update user list', users);
-
-		}
-		
+		users.push({ id: socket.id, name: socket.name });
+		userAdded = true;
+		io.emit('update user list', users);
+		socket.join('lobby');
+		currRoom = 'lobby';
+		console.log(users);
 	});
 
-	socket.on('chat message', function(msg) {
-		io.emit('chat message', msg);
+
+	socket.on('create room', function(rname) {
+		socket.join(rname);
+		currRoom = rname;
 	});
+
+
+	socket.on('chat message', function(data) {
+		io.to(currRoom).emit('chat message', data.msg);
+		history.push(data);
+	});
+
 
 	socket.on('disconnect', function() {
 		if (userAdded) {
-			delete users[socket.username];
+			var elementPos = users.map(function(x) {return x.id;}).indexOf(socket.id);
+			users.splice(elementPos, 1);
 			io.emit('update user list', users);
 		}
 		console.log(socket.username + ' has disconnected.');
@@ -74,10 +83,3 @@ http.listen(port, function(){
 	console.log('listening on ' + port);
 });
 
-function getSocketFromName(n) {
-	for (var i in io.sockets.sockets) {
-		if (io.sockets.sockets[i].username == n) {
-			return i;
-		}
-	}
-}
--- media/js/chat.js
+++ media/js/chat.js
@@ -1,31 +1,36 @@
 var socket = io();
+var currRoom = '';
 
 $('form').submit(function() {
-	socket.emit('chat message', $('#message-input').val());
+	socket.emit('chat message', { room: currRoom, from: socket.name, msg: $('#message-input').val() });
 	$('#message-input').val('');
 	return false;
 });
+
 
 socket.on('chat message', function(msg) {
 	$('#messages').append($('<li>').text(msg));
 });
 
+
 socket.on('update user list', function(users) {
 	$('#user-list').html('');
 	for (var i in users) {
-		$('#user-list').append('<li>' + i + '</li>');
-		console.log(i);
+		$('#user-list').append('<li>' + users[i].name + '</li>');
+		console.log(users[i].name);
 	}
 });
+
 
 $(function() {
 
 	$('#connect-button').on('click', function() {
 		socket.emit('new user', $('#username-input').val());
+	});
 
-		$('#login-div').addClass('inactive');
-		$('#chat-div').removeClass('inactive');
-
+	$('#newroom-button').on('click', function() {
+		socket.emit('create room', $('#newroom-input').val());
+		currRoom = $('#newroom-input').val();
 	});
 
 });
